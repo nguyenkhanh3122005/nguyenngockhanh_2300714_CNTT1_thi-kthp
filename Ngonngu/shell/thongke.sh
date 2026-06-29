797033bc347d769ab8d358a2497e725365772c5d
NguyenNgocKhanh <emailcuaban@gmail.com>
2026-06-29 09:51:56 +0700

    Xoa bai upload nham

--- Ngonngu/shell/thongke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Duong dan den file monan2.csv
-FILE_PATH="/home/$USER/nguyenngockhanh_2300714/Monan/Thucdon/monan2.csv"
-
-# Dem so dong chua chuoi "Viet Nam"
-COUNT=$(grep -c "Viet Nam" "$FILE_PATH")
-
-echo "So dong chua chuoi 'Viet Nam' la: $COUNT"
